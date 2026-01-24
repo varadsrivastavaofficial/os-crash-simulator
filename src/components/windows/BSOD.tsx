83fb9e761e9e8576e03d9994031e160e43c8d613
Varad Srivastava (Services) <varadsrivastava7@gmail.com>
2026-01-24 18:09:37 +0000

    QR Code not working

--- src/components/windows/BSOD.tsx
+++ src/components/windows/BSOD.tsx
@@ -2,6 +2,7 @@ import QRCode from '@/components/common/QRCode';
 
 export default function WindowsBSOD() {
   const stopCode = "CRITICAL_PROCESS_DIED";
+  const qrUrl = "https://www.windows.com/stopcode";
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-[#0078d4] p-8 font-segoe text-white sm:p-20">
@@ -15,12 +16,12 @@ export default function WindowsBSOD() {
         <p className="mt-8 text-2xl">0% complete</p>
 
         <div className="mt-12 flex flex-col items-start gap-4 sm:flex-row sm:items-center sm:gap-8">
-          <div className="h-28 w-28 bg-white p-1">
-            <QRCode />
+          <div className="h-28 w-28 bg-white p-1 flex items-center justify-center">
+            <QRCode url={qrUrl} />
           </div>
           <div className="text-lg">
             <p>For more information about this issue and possible fixes, visit</p>
-            <p>https://www.windows.com/stopcode</p>
+            <p>{qrUrl}</p>
             <p className="mt-4">If you call a support person, give them this info:</p>
             <p>Stop code: {stopCode}</p>
           </div>
