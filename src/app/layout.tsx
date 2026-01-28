645062642a3ce4651821c0adfb625a53e486a854
Varad Srivastava (Services) <varadsrivastava7@gmail.com>
2026-01-28 15:45:12 +0000

    change the firebase logo that is shown in tab section with some cool hac

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Blue Screen Simulator',
+  title: 'OS Failure Simulator',
   description: 'A simulated OS failure experience.',
 };
 
